6d8b41bb351d2a7ff7e8a21adf25c5b110116efc
Russell Ries <riesru@gmail.com>
2016-04-21 15:11:16 -0500

    finish exercise

--- divide.js
+++ divide.js
@@ -1,3 +1,8 @@
 // What is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder?
 
 
+for (var i = 11; i <= 5000; i++) {
+    if (!(i % 6 || i % 7 || i % 8 || i % 9 || i % 10)) {
+      console.log("i", i);
+    }
+}
